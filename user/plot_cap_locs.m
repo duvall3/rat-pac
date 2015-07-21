0fc87d9bacadd5b55c9a154d8589a54e79d22dfa
duvall3 <mjduvall@hawaii.edu>
2015-07-20 22:55:27 -0700

    improvements to plot_cap_locs.m

--- user/plot_cap_locs.m
+++ user/plot_cap_locs.m
@@ -2,13 +2,25 @@
 
 % make plot
 f_cap_locs = figure;
-p = plot3( x, y, z, 'marker', '^', 'linestyle', '--', 'color', 'k', 'markersize', 10, 'markerfacecolor', 'b' );
-% adjust plot
+s = scatter3( x, y, z, 'marker', 'o', 'MarkerFaceColor', 'b' );
+% add origin
+hold on
+o = scatter3( 0, 0, 0, 'marker', '*', 'sizedata', 625, 'cdata', [1 0 0], 'LineWidth', 3 );
+
+% labels
+xlabel 'x (cm)'
+ylabel 'y (cm)'
+zlabel 'z (cm)'
+
+% adjust limits
 ax = gca;
 limits = [xlim; ylim; zlim];
-L = [-max(abs(limits)) max(abs(limits))];
+L = [-max(abs(limits(:))) max(abs(limits(:)))];
 xlim(L); ylim(L); zlim(L);
+
+% make pretty
 set(ax, 'DataAspectRatio', [1 1 1])
+set(ax, 'xgrid', 'on', 'ygrid', 'on', 'zgrid', 'on')
 
 % make nice interactive
 cameratoolbar('SetMode', 'orbit')
