72c00cf49457e0b2e210cb69bb98727d36f098fd
duvall3 <mjduvall@hawaii.edu>
2015-07-21 15:15:12 -0700

    modifications to nCapAgents.sh

--- user/nCapAgents.sh
+++ user/nCapAgents.sh
@@ -9,12 +9,12 @@
 
 # check for 'awk' & 'bc' programs
 if [ $(which bc) ]; then isbc=true
-  else; isbc=false
+  else isbc=false
 fi
 if [ $(which awk) ]; then awkprogram=awk
   elif [ $(which gawk) ]; then awkprogram=gawk
   elif [ $(which mawk) ]; then awkprogram=mawk
-  else; echo "ERROR: No awk program found. Go download awk, gawk, or mawk from your repositories. You'll be glad you did."; exit 1
+  else echo "ERROR: No awk program found. Go download awk, gawk, or mawk from your repositories. You'll be glad you did."; exit 1
 fi
 
 
@@ -24,17 +24,14 @@ fi
 # determine total number of captures
 total_captures=$(grep "Capture: " $1 | wc -l)
 
-# make a list of all captures
-capture_list=$( $awkprogram '$1 ~ /Capture:/ {print $2}' $1 )
-
 # process full capture list into BASH array of unique capture-agent names
-agent_names=$( sort $capture_list | uniq | tr '\n' ' ' )
+agent_names=( $( $awkprogram '$1 ~ /Capture:/ {print $2}' $1 | sort | uniq ) )
 
 # get totals (and percentages, if bc is available) for each agent
 k1=0
 for agent in ${agent_names[*]}; do
-  agent_totals[$k1]=$(grep $agent $capture_list | wc -l)
-  if [ isbc = true ]; then agent_pcts[$k1]=$(echo "$agent_totals[$k1] / $total_captures" | bc -l); fi
+  agent_totals[$k1]=$(grep $agent $1 | wc -l)
+  if [ $isbc = true ]; then agent_pcts[$k1]=$(echo "${agent_totals[$k1]} / $total_captures * 100" | bc -l); fi
   ((k1++))
 done
 
@@ -42,19 +39,20 @@ done
 
 ## print results
 
-printf "\n\n"
+#printf "\n\n### Neutron Capture Information ###\n"
 
-for (( k2=0; k2<=k1; k2++ )); do
-  if [ isbc = true ]; then
-    printf "%s:\t%i (%2.1f\%\n)" $agent_names[$k2] $agent_totals[$k2] $agent_pcts[$k2]
+printf "\n\nTOTAL: %i\n" $total_captures
+
+for (( k2=0; k2<k1; k2++ )); do
+  if [ $isbc = true ]; then
+    printf "%s: %i %2.2f%%\n" ${agent_names[$k2]} ${agent_totals[$k2]} ${agent_pcts[$k2]}
   else
-    printf "%s:\t%i / %i\n" $agent_names[$k2] $agent_totals[$k2] $total_captures
+    printf "%s:\t%i / %i\n" ${agent_names[$k2]} ${agent_totals[$k2]} $total_captures
   fi
-  ((k2++))
 done
 
 printf "\n\n"
 
 
-# all pau!   )
+## all pau!   )
 exit 0
