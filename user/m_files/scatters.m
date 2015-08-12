1dbc0608d01bbeb79a6d165c019376da522e7eec
duvall3 <mjduvall@hawaii.edu>
2015-08-11 19:23:18 -0700

    corrected scatters.m to delete false rows from zero padding

--- user/m_files/scatters.m
+++ user/m_files/scatters.m
@@ -1,5 +1,14 @@
-% scatter.m -- read & graph info from a DATARUN.sc<NUM> file
+% scatter.m -- process info from a DATARUN.sc<NUM> file
 % ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ August 2015 ~ %
+
+% assuming you've already run: data_sc = dlmread( '$DATARUN.sc<NUM>', '', 1, 0 );
+
+% some neutrons will not survive to the specified number of scatters;
+% these entries must be removed, since dlmread will pad the missing columns with zeros
+%   -- we're assuming that no neutron will end up EXACTLY at the origin by coincidence
+%   -- can check this number later on by comparing, for example, 'length(x)' vs 'sum( total_scatters >= NUM )'
+rows_to_delete = find( data_sc(:,4)==0 & data_sc(:,5)==0 & data_sc(:,6)==0 );
+data_sc(rows_to_delete,:) = [];
 
 % read & convert mm to cm
 x0 = data_sc(:,1)/10;
