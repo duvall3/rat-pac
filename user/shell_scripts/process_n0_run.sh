2475a1037ef2c4f5bda2291cf67ebe23901dbbb1
duvall3 <mjduvall@hawaii.edu>
2016-03-14 10:45:33 -1000

    adding option to process_n0_run.sh to skip ROOT part

--- user/shell_scripts/process_n0_run.sh
+++ user/shell_scripts/process_n0_run.sh
@@ -3,23 +3,42 @@
 # ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ August 2015 ~ #
 
 
+## init
+
 FILENAME=$1
 NUM_EVENTS=$2
 
-# process neutron-capture information
-ROOTFILE="$FILENAME".root
-ROOTCOMMAND=$(printf "'NeutronCaptures.cxx(\"$ROOTFILE\",$NUM_EVENTS)'")
-eval "root -q -l -b $ROOTCOMMAND > \"$FILENAME\".n0.dat"
+# whether to run the ROOT part
+if [ $3 ]; then
+  ROOT_TF=$3
+else
+  ROOT_TF=true # default to true
+fi
+
+
+## process neutron-capture information
+# ROOT processing
+if [ $ROOT_TF = true ]; then
+  ROOTFILE="$FILENAME".root
+  ROOTCOMMAND=$(printf "'NeutronCaptures.cxx(\"$ROOTFILE\",$NUM_EVENTS)'")
+  eval "root -q -l -b $ROOTCOMMAND > \"$FILENAME\".n0.dat"
+elif [ $ROOT_TF = false ]; then
+  # do nothing
+else echo "Invalid parameter; ROOT_TF (3rd argument) should be either \"true\" or \"false\"." && exit 1
+fi
+# post-ROOT text processing
 n0_dat_to_txt.sh "$FILENAME".n0.dat | column -t > "$FILENAME".txt
 nCapAgents.sh "$FILENAME".n0.dat
 plot_gammas_mfile.sh $FILENAME
 n0_dat_to_sc.sh "$FILENAME".n0.dat
 n0_term_vols.sh "$FILENAME".n0.dat #"$FILENAME"
 
+
+## tidying up
 # make output directory & move all the new output files there
 mkdir $FILENAME
 mv -t $FILENAME $FILENAME.* gam/ plot_gammas.m scatters
 
 
-# all pau!  )
+## all pau!  )
 exit 0
