e0bd8185a6826dd4eaa6b7bb4e3e8e84b175db36
duvall3 <mjduvall@hawaii.edu>
2016-06-23 15:30:36 -1000

    tweak combine_runs.sh to check for awk program

--- user/shell_scripts/combine_runs.sh
+++ user/shell_scripts/combine_runs.sh
@@ -3,6 +3,15 @@
 # -- usage: combine_runs.sh <FILES> > OUTPUTFILE
 # ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ June 2016 ~ #
 
-awk -f combine_runs.awk $@ | column -t
+# check for awk program
+if [ $(which awk) ]; then awkprogram=awk
+  elif [ $(which gawk) ]; then awkprogram=gawk
+  elif [ $(which mawk) ]; then awkprogram=mawk
+  else echo "ERROR: No awk program found. Go download awk, gawk, or mawk from your repositories. You'll be glad you did.   )"; exit
+fi
+
+# run awk script
+$awkprogram -f combine_runs.awk $@ | column -t
 
 # all pau!
+exit 0
