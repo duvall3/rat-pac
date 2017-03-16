b35969af7f64b6b8c47e91560cc5d6a990e0592c
duvall3 <mjduvall@hawaii.edu>
2017-03-15 17:34:39 -0700

    cleanup

--- user/root_macros/TestFindNextTrack.cxx
+++ user/root_macros/TestFindNextTrack.cxx
@@ -11,13 +11,6 @@ RAT::DSReader r(filename);
 
 printf( "\n\n### Track IDs in %s from RAT::TrackCursor::FindNextTrack ###\n", filename );
 
-//// loop over tracks
-//Int_t num_of_events = T->GetEntries();
-//Int_t event(0);
-//for ( Int_t k=0; k<num_of_events; k++) {
-//  printf( "%i\n", n->Get
-//}
-
 // loop over events
 Int_t num_of_events = T->GetEntries();
 Int_t event;
