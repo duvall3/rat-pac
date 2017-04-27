ea1ce676cc918a2a69a8c261277d8194bea3bc4e
duvall3 <mjduvall@hawaii.edu>
2017-04-27 13:03:16 -0700

    adding filename headers to conflog.sh

--- user/shell_scripts/conflog.sh
+++ user/shell_scripts/conflog.sh
@@ -2,16 +2,22 @@
 # conflog -- brief script to record experimental & run configurations
 # ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ July 2015 ~ #
 
+echo "run.mac:"
 cat ./run.mac
 echo -e "\n\n# # # # #\n\n"
+echo "gen:"
 cat ./gen/*.mac
 echo -e "\n\n# # # # #\n\n"
+echo "geo:"
 cat ./*.geo
 echo -e "\n\n# # # # #\n\n"
+echo "ratdb:"
 cat ./*.ratdb
 echo -e "\n\n# # # # #\n\n"
+echo "setup.mac:"
 cat ./setup.mac
 echo -e "\n\n# # # # #\n\n"
+echo "init.mac:"
 cat ./init.mac
 
 exit 0
