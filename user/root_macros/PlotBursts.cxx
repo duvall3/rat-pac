5e2d8e8272ada48cf468cf1d082feb64d2c027e9
duvall3 <mjduvall@hawaii.edu>
2017-07-05 17:41:09 -0700

    adding PlotBursts.cxx

---
+++ user/root_macros/PlotBursts.cxx
@@ -0,0 +1,20 @@
+// PlotBursts -- plot results from Mar{c,k}'s SimpleEnergyDAQ triggers
+// ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ 7/2017 ~ //
+
+void PlotBursts( const char* filename ) {
+
+TTree *T = new TTree("T", "burst-trigger data");
+
+T->ReadFile( filename, "Ti/F:Tf:En" );
+
+T->Draw("En");
+c1->SetLogy(1);
+htemp->SetTitle(filename);
+htemp->SetLineColor(kBlue);
+htemp->SetLineWidth(2.);
+htemp->SetXTitle("Energy (MeV)");
+htemp->SetYTitle("Entries");
+htemp->Draw();
+
+// all pau!   )
+}
