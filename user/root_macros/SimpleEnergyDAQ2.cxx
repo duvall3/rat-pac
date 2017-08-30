e266ad1557bb4e57bd7e297a8436b4a513e9ddf7
duvall3 <mjduvall@hawaii.edu>
2017-08-30 15:07:55 -0700

    name tweak

--- user/root_macros/SimpleEnergyDAQ2.cxx
+++ user/root_macros/SimpleEnergyDAQ2.cxx
@@ -19,21 +19,21 @@ Int_t k(0);
 
 // create tree, read ASCII data, set branch addresses
 TTree* T = new TTree("T","T");
-T->ReadFile( filename, "event/I:event_start/D:wall_time/D:energy/D:energy_q/D" );
+T->ReadFile( filename, "event/I:event_time/D:wall_time/D:energy/D:energy_q/D" );
 Int_t event;
-Double_t event_start, wall_time, energy, energy_q;
+Double_t event_time, wall_time, energy, energy_q;
 T->SetBranchAddress( "event", &event );
-T->SetBranchAddress( "event_start", &event_start );
+T->SetBranchAddress( "event_time", &event_time );
 T->SetBranchAddress( "wall_time", &wall_time );
 T->SetBranchAddress( "energy", &energy );
 T->SetBranchAddress( "energy_q", &energy_q );
 
 
 // create and address new branches 
-Double_t run_start, interevent_time, event_start_adj, wall_time_adj;
+Double_t run_start, interevent_time, event_time_adj, wall_time_adj;
 T->GetEntry(0);
-run_start = event_start;
-T->Branch("event_start_adj", &event_start_adj, "event_start_adj/D");
+run_start = event_time;
+T->Branch("event_time_adj", &event_time_adj, "event_time_adj/D");
 T->Branch("wall_time_adj", &wall_time_adj, "wall_time_adj/D");
 //T->Branch("dataset_start", &dataset_start, "dataset_start/D");
 T->Branch("interevent_time", &interevent_time, "interevent_time/D");
@@ -43,7 +43,7 @@ T->Branch("interevent_time", &interevent_time, "interevent_time/D");
 Double_t time_current, time_prev;
 for (( k=0; k<number_of_events-1; k++ )) {
   T->GetEntry(k);
-  event_start_adj = event_start - run_start;
+  event_time_adj = event_time - run_start;
   wall_time_adj = wall_time - run_start;
   if (( k>0 )) {
     time_current = wall_time_adj;
