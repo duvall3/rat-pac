8617e1791080fbf2c315f9b20b94812d7b446bdc
Mark Jacob Duvall <duvall3@borax5.llnl.gov>
2017-10-07 11:32:20 -0700

    bugfix

--- user/root_macros/NeutronCaptures.cxx
+++ user/root_macros/NeutronCaptures.cxx
@@ -117,7 +117,7 @@ for (int event=1; event<=number_of_events; event++) { // event loop
 
   } else { // track terminated by some other process
       TString other_proc = n->GetProcess();
-      if ( ! other_proc.contains("Transportation") ) {
+      if ( ! other_proc.Contains("Transportation") ) {
 	cerr << "Warning: n0 track for event " << event << " terminated by " << n->GetProcess() << " instead of nCapture." << endl;
 	cout << "Warning: n0 track for event " << event << " terminated by " << n->GetProcess() << " instead of nCapture." << endl;
     //  printf( "Endpoint:\t%f %f %f\n", n->GetEndpoint().x(), n->GetEndpoint().y(), n->GetEndpoint().z() );
