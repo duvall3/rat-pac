337cbe1a0fb63c3e7ed54d4cf974768081264167
M. Duvall <duvall3@github.com>
2017-10-10 15:22:15 -0700

    updating borax/chain.sh

--- user/borax/chain.sh
+++ user/borax/chain.sh
@@ -2,18 +2,23 @@
 # chain -- generate a ROOT macro to combine all the SimpleEnergyDAQ TTrees
 # ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ 10/2017 ~ #
 
+
+# Usage:
+# 1) Run this script.
+# 2) Open ROOT and run the following commands:
+# 3) TChain ch("T");
+# 5) .x chain.cxx
+# 7) ch.Merge("<OUTPUT_FILENAME>.root");
+# 9) .x $RATROOT/Plot_SEDAQ_Results.cxx
+#10) f.Close ();
+# -- NOTE: You will have to supply the full path for $RATROOT
+
+
 echo -e "\
 // chain.cxx -- combine TChains
 // -- for use with SimpleEnergyDAQ from $RATROOT/user/root_macros/SimpleEnergyDAQ.cxx
 // Mark J. Duvall ~ mjduvall@hawaii.edu ~ 10/2017 ~ //
 
-// Usage:
-// 1) Run this script.
-// 2) Open ROOT and run the following commands:
-// 3) TChain ch(\"T\");
-// 4) TChain ch2(\"T2\");
-// 5) .x chain();
-// 6) .x plot();
 
 chain() {\n" > chain.cxx
 
@@ -22,7 +27,6 @@ for DIR in */; do
   FILE_PREFIX="$FILE_BASE/$FILE_BASE"
   FILENAME=$FILE_PREFIX"_SEDAQ.root"
   echo "ch.Add(\"$FILENAME\");" >> chain.cxx
-  echo "ch2.Add(\"$FILENAME\");" >> chain.cxx
 done
 
 echo -e "\n//all pau!   )\n}" >> chain.cxx
