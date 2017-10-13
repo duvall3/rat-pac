7708c35fa7b719372433f410152010dd0e7da81e
M. Duvall <duvall3@github.com>
2017-10-12 17:28:49 -0700

    simplified cancel_jobs.sh using option -h on squeue

--- user/borax/cancel_jobs.sh
+++ user/borax/cancel_jobs.sh
@@ -2,7 +2,7 @@
 # cancel_jobs -- cancel all jobs the current user is running on the cluster
 # ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ 10/2017 ~ #
 
-JOB_IDS=( $(squeue -u $USER | awk '$1 ~ /[[:digit:]]/ {print $1}') )
+JOB_IDS=( $(squeue -uh $USER | awk '{print $1}') )
 
 for JOB in ${JOB_IDS[*]}; do
   mjobctl -c $JOB
