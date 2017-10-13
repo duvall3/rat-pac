bd835bcb5a36452d2b0b39379d841a4f12a6f454
M. Duvall <duvall3@github.com>
2017-10-12 20:40:12 -0700

    modified avg_progress.sh to include .n0.dat portion of run

--- user/shell_scripts/avg_progress.sh
+++ user/shell_scripts/avg_progress.sh
@@ -4,17 +4,34 @@
 
 
 if [ $# -lt 1 ]; then
-  echo -e "\nUsage: avg_progress.sh <EVENTS_PER_JOB>\n"
+  echo -e "\nUsage: avg_progress.sh <EVENTS_PER_JOB> [DATFILE_TF]\n"
   exit 2
+fi
+
+if [ $2 ]; then
+  DATFILE_TF=$2
+else
+  DATFILE_TF=false
 fi
 
 EVENTS_PER_JOB=$1
 k=0
 total=0
-for FILE in *.log; do
-  k=$((k+1))
-  total=$((total+$(tail -n 1 $FILE | awk '{print $2}')))
-done
+increment=0
+
+if [ DATFILE_TF = false ]; then
+  for FILE in *.log; do
+    k=$((k+1))
+    increment=$(tail -qn 25 $FILE | awk '{print $2}')
+    total=$((total + increment))
+  done
+else
+  for FILE in *.dat; do
+    k=$((k+1))
+    increment=$(tail -qn 25 $FILE | awk '$1 ~ /Event:/ {print $2}')
+    total=$((total + increment))
+  done
+fi
 
 echo $(echo "$total / ($k*$EVENTS_PER_JOB)" | bc -l | cut -c1-5)
 
