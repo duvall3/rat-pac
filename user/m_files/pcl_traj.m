ccd5635f93f04d99169077b839df7145091e8e64
duvall3 <mjduvall@hawaii.edu>
2019-08-16 14:56:00 -0400

    axis labels

--- user/m_files/pcl_traj.m
+++ user/m_files/pcl_traj.m
@@ -1,14 +1,30 @@
 %% pcl_traj -- simple script to make a 3D plot
 %    of a particle's trajectory from an input of [X Y Z]
 
+da = [1 1 1];
+
 data = dlmread("pcl");
 x = data(:,1);
 y = data(:,2);
 z = data(:,3);
 
-f = figure;
-ax = axes;
+%f = figure;
+%ax = axes;
 
-p = plot3( x, y, z, 'marker', '.' );
+p = plot3( x, z, y, 'marker', '.', 'color', 'r' );
 daspect(da);
 
+hold on
+
+%cell_array = [20*-28 20*28 -201 201 -28 28];  % default
+cs = 1000.0;   % cube side
+cell_array = [ -cs cs -cs cs -cs cs ];   % for cube
+drawBox3d(cell_array, 'color', 'b');
+axlim = [-5000 5000]; % default
+xlim(axlim); ylim(axlim); zlim(axlim);
+
+xlabel 'x (mm)'
+ylabel 'z (mm)'
+zlabel 'y (mm)'
+
+% all pau!   )
