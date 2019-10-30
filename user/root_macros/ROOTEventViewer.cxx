0ffa574723831a02228fde679ebdeb4753902023
duvall3 <mjduvall@hawaii.edu>
2019-10-29 19:04:32 -0500

    cleaning up ROOTEventViewer.cxx; also adding _generalROOTEventViewer.cxx for possible later development

--- user/root_macros/ROOTEventViewer.cxx
+++ user/root_macros/ROOTEventViewer.cxx
@@ -20,7 +20,8 @@ TGeoManager *geo = new TGeoManager(filename+"_GM", "TGeoManager for "+filename);
 TGeoMaterial *mat = new TGeoMaterial("vacuum", 0, 0, 0);
 TGeoMedium *med = new TGeoMedium("vacuum", 1, mat);
 // top volume
-TGeoVolume* top = geo->MakeBox("Top", med, 1.2e5, 1.2e5, 1.2e5); //cm
+//TGeoVolume* top = geo->MakeBox("Top", med, 2.1e3, 2.1e3, 2.1e3); //cm
+TGeoVolume* top = geo->MakeBox("Top", med, 1.e3, 1.e3, 1.e3); //cm
 geo->SetTopVolume(top);
 
 
@@ -30,8 +31,7 @@ geo->SetTopVolume(top);
  */ 
 
 // placeholder box (e.g., cell array)
-//TGeoVolume* cell_array = geo->MakeBox("CellArray", med, 22.4, 22.4, 40.2); //cm
-TGeoVolume* cell_array = geo->MakeBox("CellArray", med, 2240, 2240, 4020); //cm -- scaled up temporarily
+TGeoVolume* cell_array = geo->MakeBox("CellArray", med, 22.4, 22.4, 40.2); //cm
 
 // finish and draw
 geo->CloseGeometry();
@@ -48,48 +48,11 @@ TView* view = can->GetView();
 
 //// PARTICLE TRACKS
 
-//// GENERAL VERSION:
-
-//// init using event 0, particle/track 0
-//Int_t primary_tracks_this_event(0), primary_id(0), track_id(0), step, stepcount;
-//Int_t track_id(0); //POSSIBLE ISSUE: Should these track ID's be forced to match RAT-PAC's?
-//RAT::DSReader r(filename);
-//RAT::DS::Root *ds = r.GetEvent(0);
-//RAT::DS::MC *mc = ds->GetMC();
-//RAT::DS::MCParticle* par;
-////RAT::DS::MCTrack* mct;
-////RAT::DS::MCTrackStep* mcts;
-//TString parname;
-
-//// generate list of primary tracks 
-//primary_tracks_this_event = mc->GetMCParticleCount();
-//for ( primary_id = 0; primary_id < primary_tracks_this_event; primary_id++ ) {
-//  par = mc->GetMCParticle(primary_id);
-//  mct = mc->GetMCTrack(primary_id);
-////track_id = geo->AddTrack( mct->GetID(), par->GetPDGCode(), par );
-//  geo->AddTrack( primary_id, par->GetPDGCode(), par );
-//}
-//// fill tracks
-//TObjArray* track_list = geo->GetListOfTracks();
-//TIter i = track_list->begin();
-//// per track:
-//Int_t track_num;
-//for ( track_num = 0; track_num < primary_tracks_this_event; track_num++ ) {
-//  stepcount = mct->GetMCTrackStepCount();
-//  TGeoTrack* track = *i;
-//  for ( step = 0; step < stepcount; step++ ) {
-//    mcts = mct->GetMCTrackStep(step);
-//    track->AddPoint( mcts->GetEndpoint().x(), mcts->GetEndpoint().y(), mcts->GetEndpoint().z(), mcts->GetGlobalTime() );
-//  }
-//}
-
 
 // ASSUME IBD -- i.e., 2 primary tracks per event (e+, n0)
 
 // define function to draw e+ and n0 tracks for a given top-level MC event
-void drawTracks( Int_t event ) {
-
-  ////// using RAT::TrackNav
+int drawTracks( Int_t event ) {
 
   // init
   Int_t step, stepcount;
@@ -111,7 +74,7 @@ void drawTracks( Int_t event ) {
   TGeoTrack* e_track = geo->GetListOfTracks()->At(0);
   for ( step = 0; step < stepcount; step++ ) {
     n = c.GoStep(step);
-    e_track->AddPoint( n->GetEndpoint().x(), n->GetEndpoint().y(), n->GetEndpoint().z(), n->GetGlobalTime() );
+    e_track->AddPoint( n->GetEndpoint().x()/10., n->GetEndpoint().y()/10., n->GetEndpoint().z()/10., n->GetGlobalTime() ); //cm
   }
   e_track->SetName(parname);
   e_track->SetLineColor(kRed);
@@ -128,46 +91,19 @@ void drawTracks( Int_t event ) {
   TGeoTrack* n_track = geo->GetListOfTracks()->At(1);
   for ( step = 0; step < stepcount; step++ ) {
     n = c.GoStep(step);
-    n_track->AddPoint( n->GetEndpoint().x(), n->GetEndpoint().y(), n->GetEndpoint().z(), n->GetGlobalTime() );
+    n_track->AddPoint( n->GetEndpoint().x()/10., n->GetEndpoint().y()/10., n->GetEndpoint().z()/10., n->GetGlobalTime() ); //cm
   }
   n_track->SetName(parname);
   n_track->SetLineColor(kBlue);
   n_track->SetLineWidth(2.0);
 
-  ////// directly accessing ds -- TRACK ID MISMATCH
-  //
-  //// positron
-  ////mct = mc->GetMCTrack(1);
-  ////parname = mct->GetParticleName();
-  ////stepcount = mct->GetMCTrackStepCount();
-  ////geo->AddTrack( 1, mct->GetPDGCode() );
-  ////TGeoTrack* e_track = geo->GetListOfTracks()->At(0);
-  ////for ( step = 0; step < stepcount; step++ ) {
-  ////  mcts = mct->GetMCTrackStep(step);
-  ////  e_track->AddPoint( mcts->GetEndpoint().x(), mcts->GetEndpoint().y(), mcts->GetEndpoint().z(), mcts->GetGlobalTime() );
-  ////}
-  ////e_track->SetName(parname);
-  ////e_track->SetLineColor(kRed);
-  ////e_track->SetLineWidth(2.0);
-  //
-  //// neutron
-  ////mct = mc->GetMCTrack(2);
-  ////parname = mct->GetParticleName();
-  ////stepcount = mct->GetMCTrackStepCount();
-  ////geo->AddTrack( 2, mct->GetPDGCode() );
-  ////TGeoTrack* n_track = geo->GetListOfTracks()->At(1);
-  ////for ( step = 0; step < stepcount; step++ ) {
-  ////  mcts = mct->GetMCTrackStep(step);
-  ////  n_track->AddPoint( mcts->GetEndpoint().x(), mcts->GetEndpoint().y(), mcts->GetEndpoint().z(), mcts->GetGlobalTime() );
-  ////}
-  ////n_track->SetName(parname);
-  ////n_track->SetLineColor(kBlue);
-  ////n_track->SetLineWidth(2.0);
-
   // draw tracks and print summary
   geo->DrawTracks("");
+  Printf( "\nIBD Track Summary for Event %i:\n", event);
   geo->GetListOfTracks()->Print();
-
+  
+  // return event number for later use (i.e., plotNextEvent or similar)
+  return event;
   } // end function drawTracks()
 
 //// all pau!   )
