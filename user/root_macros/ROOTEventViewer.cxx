9b45751a9629add0fafb8fed9cd333492cf9f604
duvall3 <mjduvall@hawaii.edu>
2019-10-30 01:42:28 -0500

    ROOTEventViewer.cxx ANNOTATIONS WORKING

--- user/root_macros/ROOTEventViewer.cxx
+++ user/root_macros/ROOTEventViewer.cxx
@@ -17,7 +17,6 @@ TObjString* dtr_path_tos = db->GetValue("DETECTOR[].experiment");
 TString dtr_path = dtr_path_tos->GetString();
 TString detector_name = dtr_path( dtr_path.Last('/')+1, dtr_path.Length() );
 detector_name.ReplaceAll("\"", "");
-detector_name.Prepend("DETECTOR: ");
 RAT::DSReader r(filename);
 gSystem->Load("libGeom");
 TGeoManager *geo = new TGeoManager(filename+"_GM", "TGeoManager for "+filename);
@@ -27,6 +26,54 @@ TGeoMedium *med = new TGeoMedium("vacuum", 1, mat);
 // top volume
 TGeoVolume* top = geo->MakeBox("Top", med, 1.e3, 1.e3, 1.e3); //cm
 geo->SetTopVolume(top);
+
+//// placeholder box (e.g., cell array)
+//TGeoVolume* cell_array = geo->MakeBox("CellArray", med, 22.4, 22.4, 40.2); //cm
+
+// target cell array
+TRegexp reg_cellarray = "target_cell_array";
+// obnoxious string manipulations to extract the database entries:
+// position
+TObjString* cellarray_pos_tos = db->GetValue("GEO[target_cell_array].position");
+TString cellarray_pos = cellarray_pos_tos->GetString();
+cellarray_pos.ReplaceAll("d", "");
+cellarray_pos.ReplaceAll("[", "");
+cellarray_pos.ReplaceAll(" ", "");
+cellarray_pos.Remove(cellarray_pos.Last(','));
+TObjArray* ca_pos_arr = cellarray_pos.Tokenize(',');
+TObjString* ca_pos_obj_x = ca_pos_arr->At(0);
+TObjString* ca_pos_obj_y = ca_pos_arr->At(1);
+TObjString* ca_pos_obj_z = ca_pos_arr->At(2);
+TString ca_pos_str_x = ca_pos_obj_x->GetString();
+TString ca_pos_str_y = ca_pos_obj_y->GetString();
+TString ca_pos_str_z = ca_pos_obj_z->GetString();
+Double_t ca_pos_x = ca_pos_str_x.Atof();
+Double_t ca_pos_y = ca_pos_str_y.Atof();
+Double_t ca_pos_z = ca_pos_str_z.Atof();
+//cout << cellarray_pos.Data() << endl; //debug
+//cout << ca_pos_x << "\t" << ca_pos_y << "\t" << ca_pos_z << endl;//debug
+// size
+TObjString* cellarray_siz_tos = db->GetValue("GEO[target_cell_array].size");
+TString cellarray_siz = cellarray_siz_tos->GetString();
+cellarray_siz.ReplaceAll("d", "");
+cellarray_siz.ReplaceAll("[", "");
+cellarray_siz.ReplaceAll(" ", "");
+cellarray_siz.Remove(cellarray_siz.Last(','));
+TObjArray* ca_siz_arr = cellarray_siz.Tokenize(',');
+TObjString* ca_siz_obj_x = ca_siz_arr->At(0);
+TObjString* ca_siz_obj_y = ca_siz_arr->At(1);
+TObjString* ca_siz_obj_z = ca_siz_arr->At(2);
+TString ca_siz_str_x = ca_siz_obj_x->GetString();
+TString ca_siz_str_y = ca_siz_obj_y->GetString();
+TString ca_siz_str_z = ca_siz_obj_z->GetString();
+Double_t ca_siz_x = ca_siz_str_x.Atof();
+Double_t ca_siz_y = ca_siz_str_y.Atof();
+Double_t ca_siz_z = ca_siz_str_z.Atof();
+//cout << cellarray_siz.Data() << endl; //debug
+//cout << ca_siz_x << "\t" << ca_siz_y << "\t" << ca_siz_z << endl;//debug
+
+// finally create cell_array TGeoVolume
+TGeoVolume* cell_array = geo->MakeBox("CellArray", med, ca_siz_x, ca_siz_y, ca_siz_z);
 
 //// load detector from db into TGeoManager
 //TIter i = db->begin();
@@ -42,19 +89,23 @@ geo->SetTopVolume(top);
 //
 //} //end db loop
 
-// placeholder box (e.g., cell array)
-TGeoVolume* cell_array = geo->MakeBox("CellArray", med, 22.4, 22.4, 40.2); //cm
-
 // finish and draw
 geo->CloseGeometry();
 top->SetLineColor(kMagenta);
-geo->SetTopVisible(kTRUE); //debug
+geo->SetTopVisible(kTRUE);
 cell_array->SetLineColor(kBlack);
 cell_array->SetLineWidth(0.5);
-TCanvas* can = new TCanvas("can", detector_name, 1000, 100, 850, 700);
+TString can_name = detector_name+", \""+filename+"\"";
+TCanvas* can = new TCanvas("can", can_name, 1000, 100, 850, 700);
 top->Draw();
 cell_array->Draw("SAME");
 TView* view = can->GetView();
+// annotations
+TLegend *gleg = new TLegend(0.01, 0.01, 0.3, 0.15);
+gleg->SetName("Geometry Legend");
+gleg->AddEntry(cell_array, "Target Cell Array", "lf");
+gleg->AddEntry(top, "Cave Walls", "lf");
+gleg->Draw();
 
 
 
@@ -113,9 +164,36 @@ int drawTracks( Int_t event ) {
   geo->DrawTracks("");
   Printf( "\nIBD Track Summary for Event %i:\n", event);
   geo->GetListOfTracks()->Print();
-  
+
+  // create legend if needed 
+  TList* can_list = can->GetListOfPrimitives();
+  TLegend* tleg = can_list->FindObject("Track Legend");
+  if ( tleg == 0x0 ) {
+    TLegend* tleg = new TLegend(0.80, 0.01, 0.99, 0.15);
+    tleg->SetName("Track Legend");
+    tleg->AddEntry(e_track, e_track->GetName());
+    tleg->AddEntry(n_track, n_track->GetName());
+    tleg->Draw();
+  } else {
+    // nothing needed
+  }
+
+  // update event label
+  TPaveText* label = can_list->FindObject("Event Label");
+  if ( label == 0x0 ) {
+    TPaveText* label = new TPaveText(-0.2, 0.8, 0.2, 0.9);
+    label->SetName("Event Label");
+  } else {
+    label->Clear();
+  }
+  TString evname = "Event: ";
+  evname += event;
+  label->AddText(evname);
+  label->Draw();
+
   // return event number for later use (i.e., plotNextEvent or similar)
   return event;
+  cout << endl;
   } // end function drawTracks()
 
 //// all pau!   )
