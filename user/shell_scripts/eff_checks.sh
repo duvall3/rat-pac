a6fa12f0bff3a690e0699bd95e14109c7a6c601e
duvall3 <mjduvall@hawaii.edu>
2020-04-03 19:33:13 -0500

    bugfix

--- user/shell_scripts/eff_checks.sh
+++ user/shell_scripts/eff_checks.sh
@@ -4,29 +4,26 @@
 #             eff_checks.sh	      to execute full processing of subdirectories
 # ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ 4/2020 ~ #
 
-# init
-DATARUN=$(basename $(pwd) /)
 
 # provide filename or let loop
 if [[ $1 ]]; then
   FULLFILE=$1
 else
+  DATARUN=$(basename $(pwd) /)
+  FULLFILE=eff_checks_$DATARUN
   # loop over subdirectories
   for DIR in */; do
     FILE=$(basename $DIR /)
     ROOTCMD="root -q -l -b 'effCheck.cxx(\"${DIR}${FILE}.root\")'"
-    eval $ROOTCMD >> eff_checks_$DATARUN
+    eval $ROOTCMD >> $FULLFILE
   done
-  FULLFILE=eff_checks_$DATARUN~
 fi
 
 # process results
-cp eff_checks_$DATARUN eff_checks_$DATARUN~
 TOTAL=$(cat $FULLFILE | awk '$1 ~ /events/ {print $4}' | add_list.sh)
-mv eff_checks_$DATARUN~ eff_checks_$DATARUN
 
 # append result
-echo -e "\n\n## TOTAL:\t\t$TOTAL ##\n\n" >> eff_checks_$DATARUN
+echo -e "\n\n## TOTAL:\t\t$TOTAL ##\n\n" >> $FULLFILE
 
 # all pau!   )
 exit 0
