82a7c49ca84b3dfaca2faba723cfb8a1e75969c5
duvall3 <mjduvall@hawaii.edu>
2020-04-03 19:20:59 -0500

    expanding eff_checks.sh

--- user/shell_scripts/eff_checks.sh
+++ user/shell_scripts/eff_checks.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 # eff_checks.sh -- calculate total efficiencies from a (batch-distributed) run
+#   -- USAGE: eff_checks.sh $FILENAME to run AWK section on file, OR:
+#             eff_checks.sh	      to execute full processing of subdirectories
 # ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ 4/2020 ~ #
 
 # init
 DATARUN=$(basename $(pwd) /)
 
-# loop over subdirectories
-for DIR in */; do
-  FILE=$(basename $DIR /)
-  ROOTCMD="root -q -l -b 'effCheck.cxx(\"${DIR}${FILE}.root\")'"
-  eval $ROOTCMD >> eff_checks_$DATARUN
-done
+# provide filename or let loop
+if [[ $1 ]]; then
+  FULLFILE=$1
+else
+  # loop over subdirectories
+  for DIR in */; do
+    FILE=$(basename $DIR /)
+    ROOTCMD="root -q -l -b 'effCheck.cxx(\"${DIR}${FILE}.root\")'"
+    eval $ROOTCMD >> eff_checks_$DATARUN
+  done
+  FULLFILE=eff_checks_$DATARUN~
+fi
 
 # process results
 cp eff_checks_$DATARUN eff_checks_$DATARUN~
-TOTAL=$(cat eff_checks_$DATARUN~ | awk '$1 ~ /events/ {print $4}' | add_list.sh)
+TOTAL=$(cat $FULLFILE | awk '$1 ~ /events/ {print $4}' | add_list.sh)
 mv eff_checks_$DATARUN~ eff_checks_$DATARUN
 
 # append result
