473ee186f963eac73a2a5e6a1730f110966903b5
duvall3 <mjduvall@hawaii.edu>
2020-04-10 16:08:53 -0500

    restructuring shell_scripts/eff_checks.sh

--- user/shell_scripts/eff_checks.sh
+++ user/shell_scripts/eff_checks.sh
@@ -5,23 +5,30 @@
 # ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ 4/2020 ~ #
 
 
-# provide filename or let loop
-if [[ $1 == "*.root" ]]; then
-  FULLFILE=$1
-elif [[ $1 && $1 != "*.root" ]]; then
-  echo -e '\
-USAGE: eff_checks.sh $FILENAME to run AWK section on file, OR:
-       eff_checks.sh	      to execute full processing of subdirectories'
-  echo "ERROR in eff_checks.sh: CHECK USAGE" >&2
-  exit 10
+# detect datarun name
+DATARUN=$(basename $(pwd) /)
+FULLFILE=eff_checks_$DATARUN
+
+# include alternate detector-volume regex if provided
+if [[ $1 ]]; then
+  ROOT_OPT_ARG=", \"$1\""
 else
-  # generate results
-  DATARUN=$(basename $(pwd) /)
-  FULLFILE=eff_checks_$DATARUN
-  # loop over subdirectories
+  ROOT_OPT_ARG=""
+fi
+
+# check whether results only need to be processed
+if [[ -f $FULLFILE ]]; then
+  # backup if file exists
+  echo "$FULLFILE exists; backing up."
+  cp $FULLFILE $FULLFILE.backup
+  echo "Backed up at $(date) by $USER via $0." >> $FULLFILE.backup
+fi
+
+# generate results if file does not exist
+if [[ ! -f $FULLFILE ]]; then
   for DIR in */; do
     FILE=$(basename $DIR /)
-    ROOTCMD="root -q -l -b 'effCheck.cxx(\"${DIR}${FILE}.root\")'"
+    ROOTCMD="root -q -l -b 'effCheck.cxx(\"${DIR}${FILE}.root\"${ROOT_OPT_ARG})'"
     eval $ROOTCMD >> $FULLFILE
   done
 fi
