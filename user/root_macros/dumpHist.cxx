1792f7cacab204e305e05eb36ee54890ef2b7686
duvall3 <duvall3@github.com>
2021-05-24 13:55:31 -0500

    fixing file operations in dumpHist.cxx

--- user/root_macros/dumpHist.cxx
+++ user/root_macros/dumpHist.cxx
@@ -1,4 +1,4 @@
-// dumpHist -- simple macro to print TH1 data to stdout
+// dumpHist -- simple macro to dump TH1 data as (x,y) pairs to stdout
 // ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ 5/21 ~ //
 
 //Copyright (C) 2021 Mark J. Duvall
@@ -17,9 +17,56 @@
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-void dumpHist( TH1* histo ) {
+//void dumpHist( TH1* histo, const char* filename = "" ) {
+void dumpHist( const char* histo_name, const char* filename = "" ) {
+
+  // get desired ROOT file
+  Bool_t newfile_tf;
+  TFile* f_initial = gFile;
+  if (filename == "") { // if no filename given, just use currently-open file
+    TFile* f = f_initial;
+    newfile_tf = kFALSE;
+    if (f_initial == 0x0) { // if there is no currently-open file, print error msg and finish
+      cerr << "Error: No file open or specified." << endl;
+      return;
+    }
+  } else { // filename is specified
+    if ( f_initial == 0x0 ) { // no file is initially open
+      TFile* f = TFile::Open(filename);
+      newfile_tf = kFALSE;
+    } else { // a file is initially open
+      if ( filename != f_initial->GetName() ) { // filename is some new file
+	TFile* f = TFile::Open(filename);
+	newfile_tf = kTRUE;
+      } else { // filename is for already-open file
+	TFile* f = f_initial;
+	newfile_tf = kFALSE;
+      } //endif -- test filename match
+    } //endif -- a file is initially open
+  } //endif -- filename given
+
+  // get specified histogram
+  TH1* h = (TH1*)f->FindObjectAny(histo_name);
+  if (h == 0x0) { // if histo_name not found
+    cerr << "Error: Specified histogram \"" << histo_name << "\" not found." << endl;
+    // re-open original file if applicable
+    if ( (newfile_tf == kTRUE) && (f_initial != 0x0) ) {
+      f->Close();
+      TFile::Open(f_initial->GetName());
+    }
+    return;
+  } //endif -- histo_name found
+
+  // MAIN
   Int_t nbins, k;
-  nbins = histo->GetNbinsX();
-  for ( k=0; k<nbins; k++ ) cout << histo->GetBinCenter(k) << "\t" << histo->GetBinContent(k) << endl;
+  nbins = h->GetNbinsX();
+  for ( k=0; k<nbins; k++ ) cout << h->GetBinCenter(k) << "\t" << h->GetBinContent(k) << endl;
+
+  // re-open original file if applicable
+  if ( (newfile_tf == kTRUE) && (f_initial != 0x0) ) {
+    f->Close();
+    TFile::Open(f_initial->GetName());
+  }
+
 // all pau!   )
 }
