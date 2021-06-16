a4d722d2fcae92e9f6355089c2ca86a77f138cd0
duvall3 <duvall3@github.com>
2021-06-16 15:09:33 -0500

    updating m_files/plot_g4_neutron_xs.m and adding resulting aux_data; also adding aux_data/re_ibd_spectrum.png

--- user/m_files/plot_g4_neutron_xs.m
+++ user/m_files/plot_g4_neutron_xs.m
@@ -1,11 +1,12 @@
-function [ps, pc] = plot_g4_neutron_xs( Z )
+function [ps, pi, pc] = plot_g4_neutron_xs( Z, keV_tf = false )
 
-% [ps, pc] = plot_g4_neutron_xs( Z ) -- plot GEANT4 neutron-interaction cross-sections
+% [ps, pi, pc] = plot_g4_neutron_xs( Z, keV_tf = false ) -- plot GEANT4 neutron-interaction cross-sections
 %   for given element
 % -- [pc, ps] are the graphics handles for the capture plot
 %      and the elastic-scattering plot, respectively
 % -- Z is the atomic number of the desired nucleus
-% -- optional isotope "I" argument (equal to nuclear mass in amu)
+% -- keV_tf: if true, plots energy in keV; otherwise, defaults to MeV
+% -- optional isotope "I" argument (equal to isotope nuclear mass in amu)
 %      may be added in the future
 % ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ 6/21 ~ %
 
@@ -33,29 +34,58 @@ if isempty(datadir)
 endif
 capfile = sprintf("%s/cap%d", datadir, Z);
 elastfile = sprintf("%s/elast%d", datadir, Z);
+inelastfile = sprintf("%s/inelast%d", datadir, Z);
 cap = dlmread(capfile, '', 2, 0);
 el = dlmread(elastfile, '', 2, 0);
+inel = dlmread(inelastfile, '', 2, 0);
+
+% prepare axis items
+en_lim = [1e-9 1e5];
+en_label = 'Neutron Energy (MeV)';
+en_therm = (1/40)*1e-6;
+en_reactor_low = 1e-3; % K_n = 1 keV
+en_reactor_high = 1e-2; % K_n = 10 keV
+xs_lim = [1e-30 1e-18];
+
+% change MeV to keV if desired
+if keV_tf == true
+  el(:,1) = el(:,1)*1e3;
+  inel(:,1) = inel(:,1)*1e3;
+  cap(:,1) = cap(:,1)*1e3;
+  en_lim = en_lim * 1e3;
+  en_label = 'Neutron Energy (keV)';
+  en_therm = en_therm*1e3;
+  en_reactor_low = en_reactor_low*1e3;
+  en_reactor_high = en_reactor_high*1e3;
+endif
 
 % MAIN
 %f = figure;
 ax = gca;
 ps = plot(el(:,1), el(:,2), 'color', 'red', 'linewidth', 2);
 hold on
+pi = plot(inel(:,1), inel(:,2), 'color', 'cyan', 'linewidth', 2);
 pc = plot(cap(:,1), cap(:,2), 'color', 'blue', 'linewidth', 2);
+set(ax, 'xscale', 'log')
+set(ax, 'yscale', 'log')
 
 % annotations
-Tstr = sprintf("Neutron Cross Sections for Atomic Number = %d", Z);
+Tstr = sprintf("Neutron Cross Sections for Z = %d", Z);
 T = title(Tstr);
-xlabel 'Neutron Energy (MeV)'
+xlabel(en_label)
 ylabel 'Cross Section (cm^{2})'
+l_therm = line([en_therm en_therm], [xs_lim(1) xs_lim(2)], 'color', 'black', 'linewidth', 1, 'linestyle', ':');
+p = patch( [en_reactor_low en_reactor_high en_reactor_high en_reactor_low], [xs_lim(2) xs_lim(2) xs_lim(1) xs_lim(1)], 'facecolor', [.5 .5 .5], 'facealpha', 0.2, 'edgecolor', 'none');
+l = legend( [ps pi pc l_therm p], {'Elastic Scattering', 'Inelastic Scattering', 'Capture', 'Thermal Energy', 'Reactor Region'} );
 set(ax, 'fontsize', 16)
-l = legend( [ps pc], {'Elastic Scattering', 'Capture'} );
 set(l, 'fontsize', 14)
 
 % adjust axes
-set(ax, 'xscale', 'log')
-set(ax, 'yscale', 'log')
-set(gca, 'xlim', [1e-7 1e5]); %MeV
-%set(gca, 'ylim', [1e-29 1e-19]); %cm^2 %%FIXME
+set(gca, 'xlim', en_lim)
+set(gca, 'ylim', xs_lim)
+%ylm = get(gca, 'ylim');
+%set(l_therm, 'ydata', [ylm(1) ylm(2)])
+%set(p, 'ydata', [ylm(2); ylm(2); ylm(1); ylm(1)])
+
 % all pau!   )
 %endfunction
