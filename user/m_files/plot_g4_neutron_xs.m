e718c69d54a4373da24bad6666995b89b272904d
duvall3 <duvall3@github.com>
2021-07-19 21:28:58 -0500

    plot_g4_neutron_xs.m almost finished

--- user/m_files/plot_g4_neutron_xs.m
+++ user/m_files/plot_g4_neutron_xs.m
@@ -1,8 +1,9 @@
-function [ps, pi, pc] = plot_g4_neutron_xs( Z, keV_tf = false )
+function [ ] = plot_g4_neutron_xs( Z )
+%function [ ] = plot_g4_neutron_xs( Z, color = 'blue', prev_handles = [], keV_tf = true )
 
-% [ps, pi, pc] = plot_g4_neutron_xs( Z, keV_tf = false ) -- plot GEANT4 neutron-interaction cross-sections
+% [pes, pis, pc] = plot_g4_neutron_xs( Z, keV_tf = true ) -- plot GEANT4 neutron-interaction cross-sections
 %   for given element
-% -- [ps, pi, pc] are the graphics handles for the elastic-scattering plot,
+% -- [pes, pis, pc] are the graphics handles for the elastic-scattering plot,
 %      the inelastic-scattering plot, and the capture plot, respectively
 % -- Z is the atomic number of the desired nucleus
 % -- keV_tf: if true, changes energy units to keV; otherwise, defaults to MeV
@@ -26,18 +27,13 @@ function [ps, pi, pc] = plot_g4_neutron_xs( Z, keV_tf = false )
 %    You should have received a copy of the GNU General Public License
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-% filesystem operations
+% filesystem init
 [status, datadir] = system("echo -n $G4NEUTRONXSDATA");
 if isempty(datadir)
   disp "Error: Environment variable $G4NEUTRONXSDATA not found. Were environment scripts sourced?"
   return
 endif
-capfile = sprintf("%s/cap%d", datadir, Z);
-elastfile = sprintf("%s/elast%d", datadir, Z);
-inelastfile = sprintf("%s/inelast%d", datadir, Z);
-cap = dlmread(capfile, '', 2, 0);
-el = dlmread(elastfile, '', 2, 0);
-inel = dlmread(inelastfile, '', 2, 0);
+[status2, datadir_basename] = system("basename $G4NEUTRONXSDATA");
 
 % prepare axis items
 en_lim = [1e-9 1e5];
@@ -46,47 +42,78 @@ en_therm = (1/40)*1e-6;
 en_reactor_low = 1e-3; % K_n = 1 keV
 en_reactor_high = 1e-2; % K_n = 10 keV
 xs_lim = [1e-30 1e-18];
+% switch to keV
+en_lim = en_lim * 1e3;
+en_label = 'Neutron Energy (keV)';
+en_therm = en_therm*1e3;
+en_reactor_low = en_reactor_low*1e3;
+en_reactor_high = en_reactor_high*1e3;
 
-% change MeV to keV if desired
-if keV_tf == true
-  el(:,1) = el(:,1)*1e3;
-  inel(:,1) = inel(:,1)*1e3;
-  cap(:,1) = cap(:,1)*1e3;
-  en_lim = en_lim * 1e3;
-  en_label = 'Neutron Energy (keV)';
-  en_therm = en_therm*1e3;
-  en_reactor_low = en_reactor_low*1e3;
-  en_reactor_high = en_reactor_high*1e3;
-endif
-
-% MAIN
-%f = figure;
-ax = gca;
+% plot prep
+f = figure;
+ax = axes('xlim', en_lim, 'ylim', xs_lim, 'xscale', 'log', 'yscale', 'log');
 set(gcf, 'name', 'GEANT4 Neutron Cross-Section Data');
-ps = plot(el(:,1), el(:,2), 'color', 'red', 'linewidth', 2);
 hold on
-pi = plot(inel(:,1), inel(:,2), 'color', 'cyan', 'linewidth', 2);
-pc = plot(cap(:,1), cap(:,2), 'color', 'blue', 'linewidth', 2);
-set(ax, 'xscale', 'log')
-set(ax, 'yscale', 'log')
+l_es = line( [en_lim(1) en_lim(1)], [xs_lim(1) xs_lim(1)], 'color', 'black' );
+l_is = line( [en_lim(1) en_lim(1)], [xs_lim(1) xs_lim(1)], 'linestyle', ':', 'color', 'black' );
+l_cap = line( [en_lim(1) en_lim(1)], [xs_lim(1) xs_lim(1)], 'linestyle', '--', 'color', 'black' );
+set( [l_es l_is l_cap], 'visible', 'off')
+l_therm = line([en_therm en_therm], [xs_lim(1)*.1 xs_lim(2)*10], 'color', 'black', 'linewidth', 1, 'linestyle', ':');
+p = patch( [en_reactor_low en_reactor_high en_reactor_high en_reactor_low], [xs_lim(2)*10 xs_lim(2)*10 xs_lim(1)*.1 xs_lim(1)*.1], 'facecolor', [.5 .5 .5], 'facealpha', 0.2, 'edgecolor', 'none');
+legend_items = [l_es l_is l_cap l_therm p];
+legend_names = {'Elastic Scattering', 'Inelastic Scattering', 'Capture', 'Thermal Energy', 'Reactor Region'};
 
-% annotations
-Tstr = sprintf("Neutron Cross Sections for Z = %d", Z);
+% labels
+Tstr = sprintf("Neutron Cross Sections from %s", datadir_basename);
 T = title(Tstr);
 xlabel(en_label)
 ylabel 'Cross Section (cm^{2})'
-l_therm = line([en_therm en_therm], [xs_lim(1) xs_lim(2)], 'color', 'black', 'linewidth', 1, 'linestyle', ':');
-p = patch( [en_reactor_low en_reactor_high en_reactor_high en_reactor_low], [xs_lim(2) xs_lim(2) xs_lim(1) xs_lim(1)], 'facecolor', [.5 .5 .5], 'facealpha', 0.2, 'edgecolor', 'none');
-l = legend( [ps pi pc l_therm p], {'Elastic Scattering', 'Inelastic Scattering', 'Capture', 'Thermal Energy', 'Reactor Region'} );
 set(ax, 'fontsize', 16)
-set(l, 'fontsize', 14)
+colorlist = 'brmgc';
+icolor = 1;
+
+% MAIN
+for z = Z
+  % read data
+  capfile = sprintf("%s/cap%d", datadir, z);
+  elastfile = sprintf("%s/elast%d", datadir, z);
+  inelastfile = sprintf("%s/inelast%d", datadir, z);
+  cap = dlmread(capfile, '', 2, 0);
+  el = dlmread(elastfile, '', 2, 0);
+  inel = dlmread(inelastfile, '', 2, 0);
+  % switch to keV
+  el(:,1) = el(:,1)*1e3;
+  inel(:,1) = inel(:,1)*1e3;
+  cap(:,1) = cap(:,1)*1e3;
+  % remove non-plottable data
+  cap(isnan(cap(:,2)),:) = [];
+  el(isnan(el(:,2)),:) = [];
+  inel(isnan(inel(:,2)),:) = [];
+  cap(cap(:,2)<=0,:) = [];
+  el(el(:,2)<=0,:) = [];
+  inel(inel(:,2)<=0,:) = [];
+  % make plots
+  pes = plot(el(:,1), el(:,2), 'linewidth', 2);
+  pes_label = sprintf('Z = %d', z);
+  set(gca, 'ylim', xs_lim)
+  pis = plot(inel(:,1), inel(:,2), 'linewidth', 2, 'linestyle', ':');
+  pc = plot(cap(:,1), cap(:,2), 'linewidth', 2, 'linestyle', '--');
+  set( [pes pis pc], 'color', sprintf('%s', colorlist(icolor)))
+  icolor += 1;
+  legend_items(end+1) = pes;
+  legend_names(end+1) = pes_label;
+end
+%set(ax, 'xscale', 'log')
+%set(ax, 'yscale', 'log')
+
+% legend
+l = legend( legend_items, legend_names, 'location', 'northeastoutside' );
 
 % adjust axes
-set(gca, 'xlim', en_lim)
-set(gca, 'ylim', xs_lim)
-%ylm = get(gca, 'ylim');
-%set(l_therm, 'ydata', [ylm(1) ylm(2)])
-%set(p, 'ydata', [ylm(2); ylm(2); ylm(1); ylm(1)])
+%set(gca, 'xlim', en_lim)
+%set(gca, 'ylim', xs_lim)
+% workaround
+disp "To fix the axes, run the following command: set(gca, 'ylim', [1e-30 1e-18])"
 
 % all pau!   )
 %endfunction
