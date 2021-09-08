b828cce1bf523cbc745c356f6b0bf4d57e84388a
duvall3 <duvall3@github.com>
2021-09-08 16:49:47 -0500

    minor tweaks to aux_data/results_plot_descriptions.txt and root_macros/SEDAQ2.cxx

--- user/root_macros/SEDAQ2.cxx
+++ user/root_macros/SEDAQ2.cxx
@@ -526,8 +526,8 @@ if ( T2->GetEntries() > 0 && kGraphics==true ) { // skip T2 graphics if there we
   hpy->SetTitle("y (mm)");
   hpz->SetTitle("z (mm)");
   hpx->SetTitleOffset(1.5);
-  hpy->SetTitleOffset(1.5);
-  hpz->SetTitleOffset(1.5);
+  hpy->SetTitleOffset(1.7);
+  hpz->SetTitleOffset(1.2);
   // delayed
   // cycle coordinates to adjust for TTree->Draw(TH3) //KEEPME//
   T2->Draw("delayed_cand_z:delayed_cand_y:delayed_cand_x>>h_delayed");
