31d2fce01e5d40006cb4c7dd959e3f5f0e49b7a3
duvall3 <duvall3@github.com>
2021-09-13 14:47:32 -0500

    adding examples/radarPlotExample.cxx

---
+++ user/root_macros/examples/radarPlotExample.cxx
@@ -0,0 +1,31 @@
+// radarPlotExample -- simple macro demonstrating an only slightly-hacked method
+//   for creating an angular 1-D histogram ("radar plot") in ROOT
+// ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ 9/2021 ~ //
+
+TH2D* radarPlotExample() {
+
+// init
+Int_t k(0), N(1000);
+TH2D *h_radar = new TH2D("h_radar", "Example Angular Histogram (degrees)", 36, -180., 180., 1, 0., 1.01);
+
+// fill
+for ( k=0; k<N; k++ ) {
+  h_radar->Fill( gRandom->Gaus(0.,45.), 1. );
+}
+
+// draw
+TCanvas *can_radar = new TCanvas("can_radar", "can_radar");
+h_radar->Draw("cyllego");
+
+//// adjust view
+//TView *view = gPad->GetView();
+//view->Top();
+
+// to adjust view
+printf("\nTo set the view, run the following lines:\n\n");
+printf("TView *view = gPad->GetView();\n");
+printf("view->Top();\n\n");
+
+// all pau!   )
+return h_radar;
+}
