e110595a9a8b83ed1cfd26347d1f87d37d35550b
duvall3 <duvall3@github.com>
2021-09-18 15:27:51 -0500

    readability improvement to scale grid in radarPlot.cxx

--- user/root_macros/util/radarPlot.cxx
+++ user/root_macros/util/radarPlot.cxx
@@ -52,7 +52,7 @@ Int_t j = 0, nScaleRings = 5, scaleStep = TMath::Nint(1.*hinMax/nScaleRings);
 //Int_t j = 0, nScaleRings = 5, scaleStep = TMath::Nint(TMath::Ceil(scaleMax/nScaleRings));
 TH2D *h_scale = new TH2D( "h_scale", "scale for radar plot", N, xlow, xup, nScaleRings, 0., scaleMax );
 h_scale->SetLineColor(kGray);
-h_scale->SetLineWidth(1.);
+h_scale->SetLineWidth(2.);
 h_scale->SetStats(0);
 for ( k=0; k<=N; k++ ) {
   for ( j=0; j<=nScaleRings; j++ ) h_scale->SetBinContent( k, j, (nScaleRings-j)*scaleStep );
