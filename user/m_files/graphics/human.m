cc43d9626df2a75fb164d595f995db4e0945729e
duvall3 <duvall3@github.com>
2022-05-28 12:41:09 -0500

    adding human.m

---
+++ user/m_files/graphics/human.m
@@ -0,0 +1,25 @@
+function [ humx, humy, humz ] = human( S )
+
+% [ humx, humy, humz ] = human( S ) -- generate / draw a "human for scale" stick figure
+% -- S is a float value for the number of Octave units equal to 1 meter
+% -- [humx, humy, humz] are coordinate matrices for the graphics object
+% ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ 05/2022 ~ %
+
+%Copyright (C) 2022 Mark J. Duvall / T. Rocks Science
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+%% all pau!   )
+%% endfunction
