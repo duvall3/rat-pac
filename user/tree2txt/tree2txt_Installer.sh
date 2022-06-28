a0eea2e1d9e559244be641040b8a419c68d225d7
duvall3 <duvall3@github.com>
2022-06-28 15:32:19 -0500

    adding an installer for tree2txt; should be the last item for this

---
+++ user/tree2txt/tree2txt_Installer.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# tree2txt_Installer.sh -- simple installer script for Mark Duvall's RATPACEventViewer
+# -- USAGE: tree2txt_Installer.sh
+# -- see https://github.com/duvall3/rat-pac/tree/collab/user/tree2txt/
+# ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ 06/2022 ~ #
+
+##Copyright (C) 2022 Mark J. Duvall
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# opening message
+echo -e '\n\n# # # Downloading and installing tree2txt.
+Files will be downloaded to $HOME/tree2txt/ and linked in the following directories:
+Shell script: $RATROOT/user/shell_scripts or $HOME/bin
+ROOT Macro: $ROOTSYS/macros or $HOME/bin
+Octave/MATLAB script: $RATROOT/user/m_files or $HOME/bin\n\n'
+
+# get/set directories
+if [ ! -d $HOME/bin ]; then mkdir $HOME/bin; fi
+INSTALL_DIR=$HOME/bin/tree2txt
+mkdir $INSTALL_DIR
+if [ -w $RATROOT/user/shell_scripts ]; then
+  SHDIR=$RATROOT/user/shell_scripts
+else
+  SHDIR=$HOME/bin
+fi
+if [ -w $ROOTSYS/macros ]; then
+  ROOTDIR=$ROOTSYS/macros
+else
+  ROOTDIR=$HOME/bin
+fi
+if [ -w $RATROOT/user/m_files ]; then
+  MDIR=$RATROOT/user/m_files
+else
+  MDIR=$HOME/bin
+fi
+
+# debug
+# echo -e "$INSTALL_DIR\t$SHDIR\t$ROOTDIR\t$MDIR\n"
+
+# download files
+GHPREFIX=https://github.com/duvall3/rat-pac/raw/collab/user/
+cd $INSTALL_DIR
+wget $GHPREFIX/tree2txt/README.md
+wget $GHPREFIX/shell_scripts/tree2txt.sh
+wget $GHPREFIX/root_macros/util/tree2txt.cxx
+wget $GHPREFIX/m_files/import_labeled_data.m
+
+# create symlinks
+ln -s $(pwd -P)/tree2txt.sh $SHDIR
+ln -s $(pwd -P)/tree2txt.cxx $ROOTDIR
+ln -s $(pwd -P)/import_labeled_data.m $MDIR
+
+# finish up
+cd -
+echo -e "\n\n# # # Installer script finished.\n\n"
+
+# all pau!   )
+exit 0
+
