3f324750dfc185151655cd4d131b29bc9b89e6cb
duvall3 <duvall3@github.com>
2024-01-07 17:12:18 -0600

    fixing OOM typo (!!) in {template,forest,cubictest}/gen/erica.mac -- neutrons @ 4vs40 keV

--- user/root_macros/util/scatPos.cxx
+++ user/root_macros/util/scatPos.cxx
@@ -17,10 +17,31 @@
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-TTree* scatPos( const char* filename = "") {
+//TTree* scatPos( const char* filename = "") {
+TList* scatPos( const char* filename = "", const Bool_t kSave = kFALSE, const char* savename = "") {
 
 
 // INIT
+
+// file operations
+if (filename=="") {
+    printf("\nEnter filename:\n");
+    cin >> filename;
+}
+const char* wd = gSystem->WorkingDirectory();
+TSystemDirectory sd( gSystem->BaseName(wd), gSystem->DirName(wd) );
+  while (! sd.GetListOfFiles()->FindObject(filename)) {
+  printf("File \"%s\" not found. Please enter another filename:\n", filename);
+  cin >> filename;
+}
+if (kSave) {
+  if (savename=="") {
+    TString saveName(filename);
+    saveName.ReplaceAll(".root", "");
+    saveName.Append("_scatt.root");
+  }
+  TFile *f = TFile::Open(saveName.Data(), "recreate");
+}
 
 // RAT init
 RAT::DSReader r(filename);
@@ -38,31 +59,46 @@ Int_t currentStep, totalSteps, currentScatter, totalScatters;
 Double_t cos_psi; 		// p_nu_hat = {-1,0,0}  -->  cos[psi] = -R.Unit().X()
 
 // TTree init
-TTree *T_sp = new TTree("T_sp", "Neutron data by number of scatters");
-T_sp->Branch("event", &event);
-T_sp->Branch("r", &r);
-T_sp->Branch("cos_psi", &cos_psi);
-
+/* TTree *T_sp = new TTree("T_sp", "Neutron data by number of scatters"); */
+TTree *Tk;
+TString treeName, treeTitle;
+Int_t nScat;
+TList *treeList = new TList;
+for (Int_t k=0; k<5; k++) {
+  nScat = nScatterSelection[k];
+  treeName.Form("T%d", nScat);
+  treeTitle.Form("neutron parameters at scatter #%d", nScat);
+  Tk = new TTree(treeName.Data(), treeTitle.Data());
+  Tk->Branch("R", &R);
+  Tk->Branch("cos_psi", &cos_psi);
+  treeList->Add(Tk);
+}
+TTree *T_summary = new TTree("T_summary", "neutron-scattering summary by event");
+T_summary->Branch("event", &event);
+T_summary->Branch("totalSteps", &totalSteps);
+T_summary->Branch("totalScatters", &totalScatters);
+T_summary->Branch("R_final", &R_final);
+T_summary->Branch("cos_psi_final", &cos_psi);
 
 // MAIN
 
 // init for main
 TString process;
 TRegexp procES("hadElastic"), procIS("neutronInelastic"), procNC("nCapture"), procND("nDecay"); //TODO CHECKME
-Int_t scatIndex, scatSelection;
+Int_t scatIndex; //, scatSelection;
 
 // event loop
 nav.Clear();
 cerr << endl << "Beginning event loop..." << endl << endl; //debug
-//for ( event = 0; event < totalEvents; event++ ) {
-for ( event = 0; event < 5; event++ ) { //debug
+for ( event = 0; event < totalEvents; event++ ) {
+//for ( event = 0; event < 5; event++ ) { //debug
 
   // event init
   ds = r.GetEvent(event);
   RAT::TrackNav nav(ds);
   c = nav.Cursor(kFALSE);
   n = c.GoChild(0);			// assume neutron-only run
-  if ( n->GetPDGCode() != 2112 ) {	// assume IBD run
+  if ( n->GetPDGCode() != 2112 ) {	// assume IBD-only run
     c.GoParent();
     n = c.GoChild(1);
   }
@@ -73,7 +109,6 @@ for ( event = 0; event < 5; event++ ) { //debug
   }
 
   // step loop
-  //TODO (Re)set nScatterSelection-specific positions to TVector3(NAN, NAN, NAN)
   currentStep = 0;
   currentScatter = 0;
   totalScatters = 0;
@@ -83,15 +118,15 @@ for ( event = 0; event < 5; event++ ) { //debug
     // check physics process
     process = n->GetProcess();
     if ( process.Contains(procES) ) {	// scatter occurred
-      //cerr << "-- Scatter Detected --" << endl; //debug
       currentScatter++;
 	for (scatIndex=0; scatIndex<5; scatIndex++) {
-	  scatSelection = nScatterSelection[scatIndex];
-	  if (currentScatter==scatSelection) {						// scatter count is one of desired selections
-	    //cerr << "\t\t\t// detected scatter # " << scatSelection << endl; //debug
+	  nScat = nScatterSelection[scatIndex];
+	  Tk = (TTree*)treeList->At(scatIndex);
+	  if (currentScatter==nScat) {						// scatter count is one of desired selections
 	    R = n->GetEndpoint();
 	    cos_psi = -R.Unit().X();
-	    printf("Event = %d\tN = %d\tx = %f\ty = %f\tz = %f\tcos_psi = %f\n", event, currentScatter, R.X(), R.Y(), R.Z(), cos_psi); //debug
+	    Tk->Fill();
+	    //printf("Event = %d\tN = %d\tx = %f\ty = %f\tz = %f\tcos_psi = %f\n", event, currentScatter, R.X(), R.Y(), R.Z(), cos_psi); //debug
 	  }
 	}
     } else if ( (process.Contains(procIS)) || (process.Contains(procNC)) ) { 		// capture occurred
@@ -106,6 +141,7 @@ for ( event = 0; event < 5; event++ ) { //debug
   if (c.IsTrackEnd()) {
     totalScatters = currentScatter;
     R_final = c.Here()->GetEndpoint();
+    T_summary->Fill();
   } else {
     gROOT->Error("scatPos.cxx | trackEnd", "Sanity check failed.");
     return 0x0;
@@ -114,13 +150,26 @@ for ( event = 0; event < 5; event++ ) { //debug
   // prevent memory leak
   nav.Clear();
 
-  //debug
-  printf("Event: %d\tSteps: %d\tScatters: %d\tFinal_cos_psi: %f\n\n", event, totalSteps, currentScatter, -R_final.Unit().X()); //debug
+  ////debug
+  //printf("Event: %d\tSteps: %d\tScatters: %d\tFinal_cos_psi: %f\n\n", event, totalSteps, currentScatter, -R_final.Unit().X());
 
 } // event loop
 
-cerr << endl << "Finished." << endl << endl; //debug
+cerr << "Finished." << endl << endl; //debug
+
+// save if desired
+if (kSave) {
+
+  // save if desired
+  if (kSave) {
+    /* treeList->Write("treeList", TObject::kSingleKey); */
+    treeList->Write();
+    T_summary->Write("T_summary");
+    f->Close();
+  }
+}
 
 // all pau!   )
-return T_sp;
+//return T_sp;
+return treeList;
 }
